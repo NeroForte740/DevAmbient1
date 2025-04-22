8fa93387461db2dcb1f00cb86559af8783f7ffed
Kelvin da Silva <kelvindasilva@edu.unifor.br>
2025-04-22 08:01:16 -0300

    Update db.js

--- project/src/config/db.js
+++ project/src/config/db.js
@@ -1,9 +1,9 @@
 const { createClient } = require("@supabase/supabase-js");
 const dotenv = require("dotenv").config();
 
-const url = "https://zbthtpozyiduplpzylos.supabase.co";
+const url = "URL";
 const apikey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpidGh0cG96eWlkdXBscHp5bG9zIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczOTQ5NTE1NSwiZXhwIjoyMDU1MDcxMTU1fQ.L8hiIx1hogoMAVTjDBgxtOby-4lyrTftSK4egG_GWzA";
+  "APIKEY";
 
 const supabase = createClient(url, apikey);
 
